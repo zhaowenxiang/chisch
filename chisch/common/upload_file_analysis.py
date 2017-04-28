3823b5b21df927a97cfeddd3eca4e6e38645d228
zhaowenxiang <zhaowenxiang@chinac.com>
2017-04-28 17:46:35 +0800

    upload

---
+++ chisch/common/upload_file_analysis.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+
+
+def parse_body_arguments(content_type, body):
+    if content_type != "multipart/form-data":
+        return {}, {}
+    if 'Content-Disposition' in body:
+        return parse_multipart_form_data(data=body)
+    else:
+        return parse_single_form_data(data=body)
+
+
+def parse_single_form_data(data=""):
+    arguments = []
+    files = [data]
+    return arguments, files
+
+
+def parse_multipart_form_data(data=""):
+    """Parses a ``multipart/form-data`` body.
+
+    The ``boundary`` and ``data`` parameters are both byte strings.
+    The dictionaries given in the arguments and files parameters
+    will be updated with the contents of the body.
+    """
+    # The standard allows for the boundary to be quoted in the header,
+    # although it's rare (it happens at least for google app engine
+    # xmpp).  I think we're also supposed to handle backslash-escapes
+    # here but I'll save that until we see a client that uses them
+    # in the wild.
+    boundary_eoh = data.find("\r\n")
+    boundary = data[:boundary_eoh]
+
+    arguments = []
+    files = []
+
+    if boundary.startswith(b'"') and boundary.endswith(b'"'):
+        boundary = boundary[1:-1]
+    final_boundary_index = data.rfind(boundary + b"--")
+    if final_boundary_index == -1:
+        return
+    parts = data[:final_boundary_index].split(
+        boundary + b"\r\n")
+    for part in parts:
+        if not part:
+            continue
+        eoh = part.find(b"\r\n\r\n")
+        if eoh == -1:
+            continue
+        content_disposition = part[:part.find(b"\r\n")]
+        disposition, disp_params = \
+            _parse_content_disposition(content_disposition)
+        if disposition != "Content-Disposition: form-data" or \
+                not part.endswith(b"\r\n"):
+            continue
+        value = part[eoh + 4:-2]
+        if not disp_params.get("name"):
+            continue
+        name = disp_params["name"]
+        if disp_params.get("filename"):
+            file_type = _parse_file_type(part)
+
+            file = {
+                'name': name,
+                'file_name': disp_params.get("filename"),
+                'file_type': file_type,
+                'value': value,
+            }
+            files.append(file)
+        else:
+            argument = {
+                'name': name,
+                'value': value,
+            }
+            arguments.append(argument)
+
+    return arguments, files
+
+
+def _parseparam(s):
+    while s[:1] == ';':
+        s = s[1:]
+        end = s.find(';')
+        while end > 0 and (s.count('"', 0, end) - s.count('\\"', 0, end)) % 2:
+            end = s.find(';', end + 1)
+        if end < 0:
+            end = len(s)
+        f = s[:end]
+        yield f.strip()
+        s = s[end:]
+
+
+def _parse_content_disposition(line):
+    """Parse a Content-type like header.
+
+    Return the main content-type and a dictionary of options.
+
+    """
+    parts = _parseparam(';' + line)
+    key = next(parts)
+    pdict = {}
+    for p in parts:
+        i = p.find('=')
+        if i >= 0:
+            name = p[:i].strip().lower()
+            value = p[i + 1:].strip()
+            if len(value) >= 2 and value[0] == value[-1] == '"':
+                value = value[1:-1]
+                value = value.replace('\\\\', '\\').replace('\\"', '"')
+            pdict[name] = value
+        else:
+            pdict[p] = None
+    return key, pdict
+
+
+def _parse_file_type(line):
+    ctype_list = []
+    ctype_eoh = line.find('Content-Type')
+    point = ctype_eoh + len('Content-Type') + 1
+    while True:
+        if line[point] in [None, ' ', ':']:
+            point += 1
+            continue
+        elif line[point] == '\r':
+            break
+        else:
+            ctype_list.append(line[point])
+            point += 1
+            continue
+    return ''.join(ctype_list)
+
--- user_/views.py
+++ user_/views.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import cgi
 import logging
-import struct
-
-import tornado.httputil
-
-from gp.fileupload import Storage
-from gp.fileupload import purge_files
 
 from django.db import transaction
+
+from chisch.common import upload_file_analysis as ufa
 
 from chisch.common.retwrapper import RetWrapper
 from chisch.common import dependency
@@ -75,75 +70,14 @@ class UserDetailView(DetailView):
 
     @transaction.atomic
     def upload_avatar(self, request, *args, **kwargs):
-        args = {}
-        files = {}
-        d = request.body
-        separator = d[6:40]
-        parse_multipart_form_data(separator, d, args, files)
+        content_type = request.content_type
+        arguments, files = ufa.parse_body_arguments(content_type=content_type,
+                                                    body=request.body)
+        for fi in files:
+            with open('/root/image/' + fi['name'], 'w') as f:
+                f.write(fi['value'])
+                f.close()
         print args, files
-
-def parse_multipart_form_data(boundary, data, arguments, files):
-    """Parses a ``multipart/form-data`` body.
-
-    The ``boundary`` and ``data`` parameters are both byte strings.
-    The dictionaries given in the arguments and files parameters
-    will be updated with the contents of the body.
-    """
-    # The standard allows for the boundary to be quoted in the header,
-    # although it's rare (it happens at least for google app engine
-    # xmpp).  I think we're also supposed to handle backslash-escapes
-    # here but I'll save that until we see a client that uses them
-    # in the wild.
-    if boundary.startswith(b'"') and boundary.endswith(b'"'):
-        boundary = boundary[1:-1]
-    final_boundary_index = data.rfind(b"--" + boundary + b"--")
-    if final_boundary_index == -1:
-        gen_log.warning(
-            "Invalid multipart/form-data: no final boundary")
-        return
-    parts = data[:final_boundary_index].split(
-        b"--" + boundary + b"\r\n")
-    for part in parts:
-        if not part:
-            continue
-        eoh = part.find(b"\r\n\r\n")
-        if eoh == -1:
-            # gen_log.warning("multipart/form-data missing headers")
-            continue
-        headers = HTTPHeaders.parse(part[:eoh].decode("utf-8"))
-        disp_header = headers.get("Content-Disposition", "")
-        disposition, disp_params = _parse_header(disp_header)
-        if disposition != "form-data" or not part.endswith(b"\r\n"):
-            # gen_log.warning("Invalid multipart/form-data")
-            continue
-        value = part[eoh + 4:-2]
-        if not disp_params.get("name"):
-            # gen_log.warning("multipart/form-data value missing name")
-            continue
-        name = disp_params["name"]
-        if disp_params.get("filename"):
-            ctype = headers.get("Content-Type", "application/unknown")
-            files.setdefault(name, []).append(HTTPFile(  # type: ignore
-                filename=disp_params["filename"], body=value,
-                content_type=ctype))
-        else:
-            arguments.setdefault(name, []).append(value)
-
-        # a, = struct.unpack('i', request.body)
-        # b = 10
-        #
-        # fields = cgi.FieldStorage(fp=request.environ['wsgi.input'],
-        #                           environ=request.environ,
-        #                           keep_blank_values=1)
-        # image_str = kwargs['image']
-        # import base64
-        # with open('/root/a.png', 'wb') as fout:
-        #     fout.write(base64.b64decode(image_str))
-        # try:
-        #     user = self.user_manager.upload_avatar()
-        # except Exception, e:
-        #     return RetWrapper.wrap_and_return(e)
-        # return RetWrapper.wrap_and_return(user)
 
 
 @dependency.requires('user_manager', 'verification_manager')
