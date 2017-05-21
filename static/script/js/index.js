46cc6cafc02d3f69a5ed9a40f130155715d8c3ca
1139005061 <1139005061@qq.com>
2017-05-21 17:29:17 +0800

    static

--- static/script/js/index.js
+++ static/script/js/index.js
@@ -1,22 +1,6 @@
 $(document).ready(function () {
-    /*设置课程，讲师查询框的默认值跟样式*/
-    var defaultSearchInputValue = "Search...";
-
-    function setDefaultSearchInput(tag) {
-        tag.val(defaultSearchInputValue).css("color", "#777")
-    }
-    function clearDefaultSearchInput(tag) {
-        tag.val("").css("color", "#fff")
-    }
-    setDefaultSearchInput($("#searchInput"))
-    $("#searchInput").focus(function () {
-       if ($("#searchInput").val() == defaultSearchInputValue) {
-           clearDefaultSearchInput($("#searchInput"))
-       }
-    });
-    $("#searchInput").blur(function () {
-       if ($("#searchInput").val() == "") {
-           setDefaultSearchInput($("#searchInput"))
-       }
-    });
+    $("#find_video_nav_ul a").click(function () {
+        $("#find_video_nav_ul a").css("color", "#888");
+        $(this).css("color", "#02f3dc");
+    })
 });
