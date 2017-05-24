64408afb4998ab4aca5dabf25acc867dcade3d4e
zhaowenxiang <zhaowenxiang@chinac.com>
2017-05-24 18:32:53 +0800

    static

--- static/script/js/index.js
+++ static/script/js/index.js
@@ -1,15 +1,3 @@
 $(document).ready(function () {
-    $("#nav-table td").click(function () {
-        console.log(window.location.href);
-        console.log(window.location.hash)
-        $(this).siblings().removeClass("header-nav-td-select");
-        $(this).addClass("header-nav-td-select");
-        var id = "#" + $(this).attr("id")
-        console.log(id);
-        $("." + $(this).attr("id") + "-nav").show()
-    });
 
-
-
-    $("#find-video").click()
 });
---
+++ static/script/route.js
@@ -0,0 +1,22 @@
+function routeConfig($routeProvider) {
+      $routeProvider.
+      when('/', {
+          controller: ListController,
+          templateUrl: 'index.html'
+      }).
+      when('/find', {
+          controller: DetailController,
+          templateUrl: 'ecommend.html'
+      }).
+      when('/my', {
+          controller: DetailController,
+          templateUrl: 'my.html'
+      }).
+      when('/friends', {
+          controller: DetailController,
+          templateUrl: 'my.html'
+      }).
+      otherwise({
+          redirectTo: '/'
+      });
+}
