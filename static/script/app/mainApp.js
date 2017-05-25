7132730bcc69e44c6c14da1c573aa0eaa1a46177
zhaowenxiang <zhaowenxiang@chinac.com>
2017-05-25 17:55:07 +0800

    static

--- static/script/app/mainApp.js
+++ static/script/app/mainApp.js
@@ -2,7 +2,7 @@ var app = angular.module('mainApp', ['ui.router']);
 app.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
     .state('/', {
-        url: '/',
+        url: '',
         templateUrl: 'view/find/main.html'
     })
     .state('my', {
